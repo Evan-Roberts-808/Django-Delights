f27072f2679b56029c0c71cb9e7254df530de3af
Evan <eroberts1998@icloud.com>
2023-11-24 18:46:41 -0500

    set up models

--- djangodelights/delights/models.py
+++ djangodelights/delights/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    available_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    recipe_requirements = models.ManyToManyField('RecipeRequirements', related_name='menu_items')
+
+    def __str__(self):
+        return self.name
+    
+class RecipeRequirements(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.quantity} {self.ingredient.name}"
+    
+class purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    purchase_time = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.menu_item.name} - {self.purchase_time}"
